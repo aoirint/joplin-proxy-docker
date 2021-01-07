e3155407fbfffb359fe6bc85fe5e5e4cffa3041a
aoirint <aoirint@gmail.com>
2021-01-08 02:12:29 +0900

    load chrome.json from script dir

--- run_macos.sh
+++ run_macos.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+
+set -eu
+
+SCRIPT_DIR=$(dirname $(realpath $0))
+
 docker run --rm -it \
   -e HTTP_PROXY \
   -e HTTPS_PROXY \
   -e DISPLAY=host.docker.internal:0 \
   --cap-add=NET_ADMIN \
   -v /dev/shm:/dev/shm \
-  -v $HOME/.config/joplin-desktop:/home/joplin/.config/joplin-desktop \
-  --security-opt seccomp=$PWD/chrome.json \
+  -v "${HOME}/.config/joplin-desktop:/home/joplin/.config/joplin-desktop" \
+  --security-opt "seccomp=${SCRIPT_DIR}/chrome.json" \
   aoirint/joplin-proxy "$@"
--- run_ubuntu.sh
+++ run_ubuntu.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+set -eu
+
+SCRIPT_DIR=$(dirname $(realpath $0))
+
 docker run --rm -it \
   -e HTTP_PROXY \
   -e HTTPS_PROXY \
@@ -7,6 +12,6 @@ docker run --rm -it \
   --cap-add=NET_ADMIN \
   -v /dev/shm:/dev/shm \
   -v /tmp/.X11-unix/:/tmp/.X11-unix/ \
-  -v $HOME/.config/joplin-desktop:/home/joplin/.config/joplin-desktop \
-  --security-opt seccomp=$PWD/chrome.json \
+  -v "${HOME}/.config/joplin-desktop:/home/joplin/.config/joplin-desktop" \
+  --security-opt "seccomp=${SCRIPT_DIR}/chrome.json" \
   aoirint/joplin-proxy "$@"
